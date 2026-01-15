7350898d9b4d77f4e4a90e5bc51c0ba745acf587
Gadaybaev Bakhtiyor <b.gadaybaev@gmail.com>
2026-01-15 16:01:49 +0500

    , allow

--- kpi_platform/kpi_platform/settings.py
+++ kpi_platform/kpi_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-s%#&!-k4(i8=4oo9cur=2t)dsk-a^+q#f+2u6mtrtbha-2u2(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
